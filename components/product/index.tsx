2dbb30eb37396349f3ebb1fd051dece3a6db63b6
20-glow <iklassi253@gmail.com>
2022-04-27 10:40:33 +0400

    first commit

---
+++ components/product/index.tsx
@@ -0,0 +1,10 @@
+
+
+
+export {default as ProductCart} from "./ProductCard"
+
+export {default as ProductView} from "./ProductView"
+
+export {default as ProductSlider} from "./ProductSlider"
+
+export {default as Swatch} from "./Swatch"
---
+++ framework/shopify/hooks.ts
@@ -0,0 +1,14 @@
+
+import { handler as useUpdateItem } from "./cart/use-update-item"
+import { handler as useAddItem } from "./cart/use-add-item"
+import { handler as useCart } from "./cart/use-cart"
+import { handler as useRemoveItem } from "./cart/use-update-item"
+
+export const shopifyHooks = {
+  cart: {
+    useAddItem,
+    useCart,
+    useRemoveItem,
+    useUpdateItem
+  }
+}
